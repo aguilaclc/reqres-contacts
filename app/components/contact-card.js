8da539169dda09407ee0e6fa446731c1cb21fd5f
Luis Carlos Aguila <lcaguila50@gmail.com>
2019-11-14 07:14:48 -0800

    Actions to open show route when clicking on picture

--- app/components/contact-card.js
+++ app/components/contact-card.js
@@ -4,6 +4,10 @@ export default Component.extend({
   classNames: ['contact-card', 'col', 's6', 'm4', 'l3'],
 
   actions: {
+    click() {
+      this.clickAction(...arguments);
+    },
+
     edit() {
       this.editAction(...arguments);
     },
