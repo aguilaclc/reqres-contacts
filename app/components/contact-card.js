1850b221a420f7d296689320f7ee61378f7e1585
Luis Carlos Aguila <lcaguila50@gmail.com>
2019-11-14 07:08:30 -0800

    Actions to edit contact

--- app/components/contact-card.js
+++ app/components/contact-card.js
@@ -2,4 +2,10 @@ import Component from '@ember/component';
 
 export default Component.extend({
   classNames: ['contact-card', 'col', 's6', 'm4', 'l3'],
+
+  actions: {
+    edit() {
+      this.editAction(...arguments);
+    },
+  },
 });
